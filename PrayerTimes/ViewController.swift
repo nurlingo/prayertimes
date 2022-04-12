731f669fc33bd506ff907a42593f6ab148bae082
Nursultan Askarbekuly <nursultan@outlook.com>
2022-04-12 19:55:36 +0300

    Initial Commit

---
+++ PrayerTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrayerTimes
+//
+//  Created by Daniya on 12/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
